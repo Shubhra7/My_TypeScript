057eed9dc615d6e08c151cf1294478ed63ede96e
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-23 00:50:52 +0530

    Add initial implementation of myArrays with superHeros, heroPower, allUsers, and MlModels

---
+++ 04_arrays/myArrays.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+// const heroPower: number[] =[]
+var heroPower = []; // another technique
+superHeros.push("spiderman");
+heroPower.push(2);
+var allUsers = [];
+allUsers.push({ name: "", isActive: true });
+// seen:3
+var MlModels = [
+    [255, 255, 255],
+    []
+];
---
+++ 04_arrays/myArrays.ts
@@ -0,0 +1,25 @@
+const superHeros: string[] =[]
+// const heroPower: number[] =[]
+const heroPower: Array<number> =[]  // another technique
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+
+// seen: 2
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+allUsers.push({name: "", isActive: true})
+
+// seen:3
+const MlModels: number[][] = [
+    [255,255,255],
+    []
+]
+
+export {}
