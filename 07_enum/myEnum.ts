40627c730d0498bc0c53f7a0361837d10f51413d
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-24 00:07:33 +0530

    Add initial implementation of SeatChoice enum with example usage in myEnum.ts and myEnum.js

---
+++ 07_enum/myEnum.ts
@@ -0,0 +1,17 @@
+// enum used for key-pair value getting 
+
+enum SeatChoice { 
+    AISLE="aisle",
+    MIDDLE=3,
+    WINDOW,
+    FOURTH
+}
+
+const hcSeat = SeatChoice.AISLE
+
+console.log(hcSeat);
+const kalu = SeatChoice.WINDOW
+console.log(kalu);
+
+
+export {}
