82a6d41c4557fb13413c512eb524b40bc0c5975b
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-23 23:53:46 +0530

    Add initial implementation of tuple type definitions and example usage in myTupEnums.js

---
+++ 06_tuple/myTupEnums.js
@@ -0,0 +1,9 @@
+"use strict";
+// const user: (string | number)[] = [1,"hc"]
+Object.defineProperty(exports, "__esModule", { value: true });
+// to fix the order of the data
+var tUser;
+tUser = ["hc", 131, true];
+var rgb = [244, 123, 122];
+var newUser = [122, "exm@gg.com"];
+newUser[1] = "hb.ji@com";
