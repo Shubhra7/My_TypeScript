ad5ab0416f304f82d0711f8e43f64c4c6855e478
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-24 00:21:50 +0530

    Enhance User interface by adding githubToken property and extend User interface with Admin role definitions

--- 08_interface/myInterface.ts
+++ 08_interface/myInterface.ts
@@ -8,7 +8,18 @@ interface User {
     getCoupon(couponname: string):number
 }
 
+// Reopening of interface
+interface User {
+    githubToken: string
+}
+
+// inheritance in interface
+interface Admin extends User{
+    role: "admin" | "ta" | "learner"
+}
+
 const hitesh: User = { dbId: 22, email: "h@h.com", userId: 2211,
+githubToken: "3345",
     startTrail: ()=>{
         return "trail Started"
     },
