a591f6b35f90479d1152f639883a4b40a6df3054
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-23 23:51:30 +0530

    Add tuple type definitions and example usage in myTupEnums.ts

---
+++ 06_tuple/myTupEnums.ts
@@ -0,0 +1,17 @@
+// const user: (string | number)[] = [1,"hc"]
+
+
+// to fix the order of the data
+let tUser: [string, number, boolean]
+tUser = ["hc", 131, true]
+
+let rgb: [number,number,number] = [244,123,122]
+
+
+// tuple
+type User = [number, string]
+const newUser:User = [122,"exm@gg.com"]
+
+newUser[1]="hb.ji@com"  
+
+export {}
