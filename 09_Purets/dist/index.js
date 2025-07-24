af44d2252f11a2f059fecc27b624c8f3d2163434
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-24 13:20:11 +0530

    Refactor User class constructor to include userID and update city initialization introduce the access specifier like public private etc

--- 09_Purets/dist/index.js
+++ 09_Purets/dist/index.js
@@ -1,9 +1,19 @@
 "use strict";
+// class User {
+//     public email:string
+//     name: string
+//     private readonly city: string= "Jaipu"
+//     constructor(email:string, name: string){
+//         this.email=email;
+//         this.name= name
+//     }
+// }
 class User {
-    constructor(email, name) {
-        this.city = "Jaipu";
+    constructor(email, name, userID) {
         this.email = email;
         this.name = name;
+        this.userID = userID;
+        this.city = "Kolkata";
     }
 }
-const Bubai = new User("Bubai", "kalu");
+const Bubai = new User("Bubai@g.com", "kalu", 123);
