3c25faba7234bd2079cca4582f04931894036839
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-24 13:46:00 +0530

    Implement Instagram and Youtube classes with TakePhoto and Story interfaces

---
+++ 09_Purets/dist/second.js
@@ -0,0 +1,21 @@
+"use strict";
+class Instagram {
+    constructor(// need all parent one to do
+    cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burst, short // can add more items
+    ) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+    createStory() {
+        console.log("Story was created");
+    }
+}
---
+++ 09_Purets/src/second.ts
@@ -0,0 +1,30 @@
+interface TakePhoto {
+    cameraMode: string
+    filter: string
+    burst: number
+}
+
+interface Story {
+    createStory(): void
+}
+
+class Instagram implements TakePhoto{
+    constructor(  // need all parent one to do
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){}
+}
+
+class Youtube implements TakePhoto, Story{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number,
+        public short: number  // can add more items
+    ){}
+
+    createStory(): void {
+        console.log("Story was created");
+    }
+}
