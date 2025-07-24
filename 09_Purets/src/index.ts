670c577cd68faca651fc7678d14d8d680fd57d70
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-24 13:20:37 +0530

    done

--- 09_Purets/src/index.ts
+++ 09_Purets/src/index.ts
@@ -1,11 +1,22 @@
+// class User {
+//     public email:string
+//     name: string
+//     private readonly city: string= "Jaipu"
+//     constructor(email:string, name: string){
+//         this.email=email;
+//         this.name= name
+//     }
+// }
+
 class User {
-    email:string
-    name: string
-    readonly city: string= "Jaipu"
-    constructor(email:string, name: string){
-        this.email=email;
-        this.name= name
+    readonly city: string = "Kolkata"
+    constructor(
+        public email:string, 
+        public name: string,
+        private userID: number
+    ){
+       
     }
 }
 
-const Bubai = new User("Bubai","kalu")
+const Bubai = new User("Bubai@g.com","kalu",123)
